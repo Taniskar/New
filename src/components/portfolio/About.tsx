749fb41800fd6230982c21be76a92ac2539f87ef
Hiren Patel <hirenpatel.ds@gmail.com>
2025-06-05 12:22:17 +0530

    Update About page

--- src/components/portfolio/About.tsx
+++ src/components/portfolio/About.tsx
@@ -10,10 +10,16 @@ export default function About() {
         About Me
       </h2>
       <p className="mb-4">
-        3+ years of experience in Data Science, Aveva PI Systems, Business Analysis & Lecturing.
+        3+ years of experience in Aveva PI Systems Professional,Technical Manager, Business Analysis & Lecturing.
       </p>
       <p className="mb-4">
-        Worked with enterprise clients like Petronet LNG, IOCL, Uber, and GAIL.
+        Expertise in Data Analytics, Data Visualization, and developing end-to-end data pipelines, leveraging tools such as Aveva PI Server, PI System Explorer, Asset Framework, Data Archive (SMT), PI vision, Aveva PI Administration, Pandas, Scikit-learn, TensorFlow, and Tableau to deliver actionable insights.
+      </p>
+      <p className="mb-4">
+        Worked with enterprise clients like Petronet LNG, IOCL, and GAIL.
+      </p>
+      <p className="mb-4">
+        Proven track record of leading cross-functional teams to deliver complex projects, including real-time data monitoring platforms, predictive analytics, and business intelligence solutions for enterprise-level organizations.
       </p>
       <p className="mb-4">
         Strong in Aveva PI System, PI Server, Data Archive, PI System Explorer, PI Vision, Machine Learning, Real-time Data Monitoring, Power BI, and AI Dashboards.
